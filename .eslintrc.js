fdd4abf317da955af25266f06f348e5f7ad8bc59
Exo Des <exod3s@gmail.com>
2020-10-17 23:52:28 +0800

    Create vue base with tailwindcss

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:vue/vue3-recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    Header
+  </header>
+  <main>
+    Main
+  </main>
+  <footer>
+    Footer
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  min-width: 100vh;
+  min-height: 100vh;
+}
+</style>
