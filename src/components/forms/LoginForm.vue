ad81117e440031a7a9f7632bb919e986e9d08eae
Exo Des <exod3s@gmail.com>
2020-10-18 19:23:04 +0800

    Rename form component

---
+++ src/components/forms/LoginForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container mx-auto py-10 w-1/2">
+    <div class="bg-white shadow-lg px-32 py-16">
+      <h4 class="uppercase text-lg font-bold mb-10">
+        Fill your login details below
+      </h4>
+      <form action="">
+        <div class="">
+          <label
+            class="font-semibold" 
+            for="username"
+          >
+            Email:
+          </label>
+          <input
+            id="username"
+            v-model.lazy="email"
+            class="border-b-2 w-full text-center"
+            type="email"
+          >
+        </div>
+        <div class="mt-4">
+          <label
+            class="font-semibold" 
+            for="password"
+          >
+            Password:
+          </label>
+          <input
+            id="password"
+            v-model.lazy="password"
+            class="border-b-2 w-full text-center"
+            type="password"
+          >
+        </div>
+        <div class="mt-16">
+          <button class="button-text">
+            Log In
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/forms/RegisterForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container mx-auto py-10 w-1/2">
+    <div class="bg-white shadow-lg px-32 py-16">
+      <h4 class="uppercase text-lg font-bold mb-10">
+        Fill the registration form below
+      </h4>
+      <form @submit="register">
+        <div class="">
+          <label
+            class="font-semibold"
+            for="username"
+          > Email: </label>
+          <input
+            id="username"
+            v-model.lazy="username"
+            class="border-b-2 w-full text-center"
+            type="email"
+          >
+        </div>
+        <div class="mt-4">
+          <label
+            class="font-semibold"
+            for="password"
+          > Password: </label>
+          <input
+            id="password"
+            v-model.lazy="password"
+            class="border-b-2 w-full text-center"
+            type="password"
+          >
+        </div>
+        <div class="mt-16">
+          <button class="button-text">
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive, ref, watch } from 'vue';
+import { register } from '@/firebase/config';
+
+export default {
+  name: 'RegisterForm',  setup(props, ctx) {
+    const loginStatus = ref(false);
+    const username = ref([]);
+    const password = ref([]);
+
+    // const register = (username. password) => {
+    //   register(username.value, password.value ).then(
+    //     (data) => {
+    //       console.log(data);
+    //     }
+    //   );
+    // };
+
+    return { username, password, loginStatus };
+  },
+};
+</script>
