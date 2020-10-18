5c4036154da243cee4215d4b98d80d85d62ec645
Exo Des <exod3s@gmail.com>
2020-10-18 19:23:23 +0800

    Clean code

--- src/components/navbar/LogIn.vue
+++ src/components/navbar/LogIn.vue
@@ -1,15 +1,13 @@
 <template>
   <li class="px-3 py-2 ml-2 rounded-lg transition duration-300 ease-in-out relative">
     <router-link to="/">
-      <a href="#">
-        <fa
-          icon="cart-plus"
-          class="w-6 h-6 m-auto"
-        />
-        <div class="absolute top-0 right-0 rounded-full bg-red-700 text-white text-xs leading-none w-5 h-5 flex">
-          <span class="m-auto">1</span>
-        </div>
-      </a>
+      <fa
+        icon="cart-plus"
+        class="w-6 h-6 m-auto"
+      />
+      <div class="absolute top-0 right-0 rounded-full bg-red-700 text-white text-xs leading-none w-5 h-5 flex">
+        <span class="m-auto">1</span>
+      </div>
     </router-link>
   </li>
   <li class="px-6 py-2">
--- src/components/navbar/LogOut.vue
+++ src/components/navbar/LogOut.vue
@@ -1,12 +1,12 @@
 <template>
   <li class="px-6 py-2">
-    <router-link to="/">
-      <a href="#">Login</a>
+    <router-link to="/Login">
+      Login
     </router-link>
   </li>
   <li class="px-6 py-2 ml-2 border-2 border-white rounded-lg transition duration-300 ease-in-out hover:bg-white hover:text-green-600">
-    <router-link to="/">
-      <a href="#">Register</a>
+    <router-link to="/Register">
+      Register
     </router-link>
   </li>
 </template>
