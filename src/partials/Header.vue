84230fcce4b014de97fcf6bfbcf387d27ef50868
Exo Des <exod3s@gmail.com>
2020-10-18 14:05:07 +0800

    Restructure header

--- src/partials/Header.vue
+++ src/partials/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="bg-green-600 w-screen">
+  <header class="bg-green-600">
     <div class="container mx-auto py-4 flex justify-between">
       <div class="logo w-64 my-auto">
         <img
@@ -8,15 +8,26 @@
         >
       </div>
       <div class="menu my-auto">
-        <ul class="flex flex-row text-white">
+        <ul class="flex flex-row text-white font-bold">
           <li class="px-6 py-2">
             <a href="#">Login</a>
           </li>
-          <li class="px-6 py-2">
-            <a href="#">Logout</a>
-          </li>
-          <li class="px-6 py-2 ml-2 border border-white rounded-lg transition duration-300 ease-in-out hover:bg-white hover:text-green-600">
+          <li class="px-6 py-2 ml-2 border-2 border-white rounded-lg transition duration-300 ease-in-out hover:bg-white hover:text-green-600">
             <a href="#">Register</a>
+          </li>
+          <li class="px-3 py-2 ml-2 rounded-lg transition duration-300 ease-in-out relative">
+            <a href="#">
+              <fa
+                icon="cart-plus"
+                class="w-6 h-6 m-auto"
+              />
+            </a>
+            <div class="absolute top-0 right-0 rounded-full bg-red-700 text-white text-xs leading-none w-5 h-5 flex">
+              <span class="m-auto">1</span>
+            </div>
+          </li>
+          <li class="px-6 py-2">
+            <a href="#">Username</a>
           </li>
         </ul>
       </div>
