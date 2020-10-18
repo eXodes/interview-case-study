0c8c91c637144886400f7f014869f02dda279bde
Exo Des <exod3s@gmail.com>
2020-10-18 14:06:56 +0800

    Add vuex

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import '@/assets/styles/index.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import { FontAwesomeIcon } from '@/plugins/font-awesome';
+import store from './store';
 
 createApp(App)
+  .use(store)
   .component('fa', FontAwesomeIcon)
   .mount('#app');
