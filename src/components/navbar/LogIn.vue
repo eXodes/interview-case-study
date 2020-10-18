c4a8eba2be7c3ad0fc0ba07f8cdf838d63f09b59
Exo Des <exod3s@gmail.com>
2020-10-18 16:39:17 +0800

    Split navbar intro separate component

---
+++ src/components/navbar/LogIn.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="px-3 py-2 ml-2 rounded-lg transition duration-300 ease-in-out relative">
+    <router-link to="/">
+      <a href="#">
+        <fa
+          icon="cart-plus"
+          class="w-6 h-6 m-auto"
+        />
+        <div class="absolute top-0 right-0 rounded-full bg-red-700 text-white text-xs leading-none w-5 h-5 flex">
+          <span class="m-auto">1</span>
+        </div>
+      </a>
+    </router-link>
+  </li>
+  <li class="px-6 py-2">
+    <a href="#">Username</a>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'LogIn'
+};
+</script>
+
+<style>
+
+</style>
--- src/partials/Header.vue
+++ src/partials/Header.vue
@@ -11,32 +11,8 @@
       </router-link>
       <div class="menu my-auto">
         <ul class="flex flex-row text-white font-bold">
-          <li class="px-6 py-2">
-            <router-link to="/">
-              <a href="#">Login</a>
-            </router-link>
-          </li>
-          <li class="px-6 py-2 ml-2 border-2 border-white rounded-lg transition duration-300 ease-in-out hover:bg-white hover:text-green-600">
-            <router-link to="/">
-              <a href="#">Register</a>
-            </router-link>
-          </li>
-          <li class="px-3 py-2 ml-2 rounded-lg transition duration-300 ease-in-out relative">
-            <router-link to="/">
-              <a href="#">
-                <fa
-                  icon="cart-plus"
-                  class="w-6 h-6 m-auto"
-                />
-                <div class="absolute top-0 right-0 rounded-full bg-red-700 text-white text-xs leading-none w-5 h-5 flex">
-                  <span class="m-auto">1</span>
-                </div>
-              </a>
-            </router-link>
-          </li>
-          <li class="px-6 py-2">
-            <a href="#">Username</a>
-          </li>
+          <LogIn />
+          <LogOut />
         </ul>
       </div>
     </div>
@@ -44,7 +20,14 @@
 </template>
 
 <script>
+import LogIn from '@/components/navbar/LogIn';
+import LogOut from '@/components/navbar/LogOut';
+
 export default {
   name: 'Header',
+  components: {
+    LogIn,
+    LogOut
+  },
 };
 </script>
