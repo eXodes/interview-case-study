492fa71f42f6f93c460f9ff8535fc13217b1a590
Exo Des <exod3s@gmail.com>
2020-10-24 04:51:16 +0800

    Fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,18 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended'],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
   parserOptions: {
     parser: 'babel-eslint',
     ecmaVersion: 2018,
     sourceType: 'module',
   },
   rules: {
-    'vue/max-attributes-per-line': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/max-attributes-per-line': 'error',
     quotes: ['error', 'single'],
-    semi: ['error', 'always'],
+    'vue/html-self-closing': 'off',
+    'vue/max-attributes-per-line': 1,
   },
 };
