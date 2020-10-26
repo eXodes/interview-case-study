55de311d8280f8010013868ed18e34028c2e3a57
Muhammad Zulthaqif <zulthaqif@prodigious.com>
2020-10-27 06:20:13 +0800

    Update base appp

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <Header />
+  <Header/>
   <router-view />
-  <footer>
-    Footer
-  </footer>
 </template>
 
 <script>
-import Header from '@/partials/Header';
+import Header from '@/views/partials/Header';
+
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
   },
 };
 </script>
 
-<style>
+<style lang="scss">
 body {
-  -ms-overflow-style: none;  /* IE and Edge */
-  scrollbar-width: none;  /* Firefox */
-  
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
 }
 
 body::-webkit-scrollbar {
@@ -40,34 +37,16 @@ body::-webkit-scrollbar {
 }
 
 .line-clamp {
-    display: -webkit-box;
+  display: -webkit-box;
   -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;  
+  -webkit-box-orient: vertical;
   overflow: hidden;
 }
 
-a:focus, button:focus, input:focus {
+a:focus,
+button:focus,
+input:focus {
   outline: none;
   box-shadow: none;
-}
-
-.button-icon {
-  @apply bg-green-600 text-white rounded-md border-2 border-green-600 px-1 py-1 ;
-}
-
-.button-icon:hover {
-  @apply bg-green-700 border-green-700;
-}
-
-.button-text {
-  @apply bg-green-600 text-white font-semibold uppercase text-sm rounded-md border-2 transition-all duration-200 ease-in-out border-green-600 px-6 py-2;
-}
-
-.button-text:hover {
-  @apply bg-green-700 border-green-700;
-}
-
-.button-small {
-  @apply px-3 py-1;
 }
 </style>
