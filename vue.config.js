502acb626491f40b8aebdb3c280bee019abe5f7e
Muhammad Zulthaqif <zulthaqif@prodigious.com>
2020-10-27 11:04:53 +0800

    Set PWA

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,18 @@ module.exports = {
     msTileColor: '#38a169',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'white',
-
+    iconPaths: {
+      favicon32: 'img/icons/icon.png',
+      favicon16: 'img/icons/icon.png',
+      appleTouchIcon: 'img/icons/icon.png',
+      maskIcon: 'img/icons/icon.png',
+      msTileImage: 'img/icons/icon.png',
+    },
     // configure the workbox plugin
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
+      swSrc: './src/registerServiceWorker.js',
     },
   },
 };
